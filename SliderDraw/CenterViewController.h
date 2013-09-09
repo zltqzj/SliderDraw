f4623e22e427158da4520755dc105ed2b6d8f42c
zltqzj <zltqzj@163.com>
2013-09-09 15:11:23 +0800

    ios抽屉效果
    左右效果都有

---
+++ SliderDraw/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  SliderDraw
+//
+//  Created by Sinosoft on 9/9/13.
+//  Copyright (c) 2013 com.Sinosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
---
+++ SliderDraw/LeftViewController.m
@@ -0,0 +1,39 @@
+//
+//  LeftViewController.m
+//  SliderDraw
+//
+//  Created by Sinosoft on 9/9/13.
+//  Copyright (c) 2013 com.Sinosoft. All rights reserved.
+//
+
+#import "LeftViewController.h"
+
+@interface LeftViewController ()
+
+@end
+
+@implementation LeftViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor orangeColor]];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
